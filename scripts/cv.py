abc9b4afd71a8769e04df2736eeb00bab6ce2c45
DovnarAlexander <29893487+DovnarAlexander@users.noreply.github.com>
2020-09-08 16:07:16 +0300

    wip

---
+++ scripts/cv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import pdfcrowd
+
+try:
+    client = pdfcrowd.HtmlToPdfClient(os.getnev("PDF_USER"), os.getnev("PDF_TOKEN"))
+    client.convertUrlToFile('https://dovnar-alexander.xyz/cv.html', 'cv.pdf')
+except pdfcrowd.Error as why:
+    sys.stderr.write('Pdfcrowd Error: {}\n'.format(why))
+    raise
