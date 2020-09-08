4b0aab1a53b386ebf37a6150e041d1367a91ff31
DovnarAlexander <29893487+DovnarAlexander@users.noreply.github.com>
2020-09-08 18:46:39 +0300

    fixe

--- scripts/cv.py
+++ scripts/cv.py
@@ -6,7 +6,7 @@ import os
 import pdfcrowd
 
 try:
-    client = pdfcrowd.HtmlToPdfClient(os.getnev("PDF_USER"), os.getnev("PDF_TOKEN"))
+    client = pdfcrowd.HtmlToPdfClient(os.getenv("PDF_USER"), os.getenv("PDF_TOKEN"))
     client.convertUrlToFile('https://dovnar-alexander.xyz/cv.html', 'cv.pdf')
 except pdfcrowd.Error as why:
     sys.stderr.write('Pdfcrowd Error: {}\n'.format(why))
